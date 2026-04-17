7f753fcc7d1fb015a0cf6bd6497394950d3a9097
Богдан Колганов <kolganov_25@mail.ru>
2026-04-17 11:01:08 +0700

    Добавьте файлы проекта.

---
+++ TMP_Laba4_Server/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace TMP_Laba4_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        }
+    }
+}
